a0000fc3c4a14d5e9bc7d84baa926d0c4fa404ee
Vishwanatha HM <vishwanathovi@gmail.com>
2025-12-16 21:01:25 +0530

    Add IST timezone configuration

--- src/config.py
+++ src/config.py
@@ -1,6 +1,16 @@
 """
 Configuration constants for the reminder system.
 """
+import pytz
+from datetime import datetime
+
 # Twilio phone number to use for all calls
 TWILIO_FROM_NUMBER = "+16108313946"
+
+# Timezone for all reminders (IST - Indian Standard Time)
+IST = pytz.timezone('Asia/Kolkata')
+
+def get_ist_now():
+    """Get current datetime in IST timezone."""
+    return datetime.now(IST)
 
